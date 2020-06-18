0d54fb70cf1355cbbc2cd1aa6f0e03201b3f23d3
Nikolay <terleew2013@yandex.ru>
2020-06-18 16:19:57 +0500

    ...

--- authorization.php
+++ authorization.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
     if($_COOKIE['auth'] == 'true') { //Получаем куки и проверяем авторизован ли пользователь
         header('Location: http://as-ps.ru/index.php');  //Если авторизован, то перенаправляем на главную страницу
     }
